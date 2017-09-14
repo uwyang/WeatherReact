491964ffe7928ed54c12ab83f40fe178a801f41a
penpen <uwyang@gmail.com>
2017-09-14 14:04:04 -0400

    using foundation to pretty up the menu bar.

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -18,20 +18,62 @@ var Nav = React.createClass({
 });
 */
 
-var Nav = (props)=> {
-  return (
-    <div>
-      nav comp
-      {/*
-        for CSS, camel case everything!
-        e.g. font-weight ---> fontWeight.
-      */}
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}} >Get Weather</IndexLink>
-      <Link to="about" activeClassName="active"  activeStyle={{fontWeight:'bold'}} >About</Link>
-      <Link to="examples" activeClassName="active"  activeStyle={{fontWeight:'bold'}} >Examples</Link>
-      <a href="#/about" activeClassName="active" >about </a>
-    </div>
-  );
-};
+var Nav = React.createClass({
+  render: function(){
+    return (
+      //in jsx, class is a keyWord.
+      //so use classname for css instread.
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">
+              React Weather app
+            </li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}} >Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="about" activeClassName="active"  activeStyle={{fontWeight:'bold'}} >About</Link>
+            </li>
+            <li>
+              <Link to="examples" activeClassName="active"  activeStyle={{fontWeight:'bold'}} >Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search Weather" />
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get Weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    );
+  },
+  onSearch: function(e){
+    e.preventDefault();
+    console.log("not wired up. ");
+  }
+});
+
+
+var old = (
+  <div>
+    nav comp
+    {/*
+      for CSS, camel case everything!
+      e.g. font-weight ---> fontWeight.
+    */}
+    <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}} >Get Weather</IndexLink>
+    <Link to="about" activeClassName="active"  activeStyle={{fontWeight:'bold'}} >About</Link>
+    <Link to="examples" activeClassName="active"  activeStyle={{fontWeight:'bold'}} >Examples</Link>
+    <a href="#/about" activeClassName="active" >about </a>
+  </div>
+);
 
 module.exports=Nav;
