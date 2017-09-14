8851473cd6c585624f8f94fb81a9f3554c26a096
penpen <uwyang@gmail.com>
2017-09-13 23:56:05 -0400

    updating middleware so it works on localhost aswell.

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 var express = require('express');
-
+//var http = require('http');
 // Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
+  if (req.headers['x-forwarded-proto'] ===  'https') {
+    var url = 'http://' + req.hostname + req.url;
+    //console.log("using https or ", req.headers['x-forwarded-proto'] , ", redirecting to http");
+    //console.log("URL: " , url);
+    res.redirect(url);
   } else {
-    res.redirect('http://' + req.hostname + req.url);
+    next();
   }
 });
 
 app.use(express.static('public'));
 
+//var server = http.createServer(app);
+//server.listen(PORT, function () {
 app.listen(PORT, function () {
   console.log('Express server is up on port ' + PORT);
 });
