689dec4d44c74a45b518bd9441a5c9254bb8c67f
penpen <uwyang@gmail.com>
2017-09-14 21:47:39 -0400

    adding fix Foundation and React compatibility issue in ErrorModal

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer= require('react-dom/server');
 
 var ErrorModal = React.createClass({
   getDefaultProps: function () {
@@ -8,18 +10,13 @@ var ErrorModal = React.createClass({
   },
   propTypes: {
       //set up porps and their default types.
-      //and if they're optional or required. 
+      //and if they're optional or required.
       title: React.PropTypes.string,
       message: React.PropTypes.string.isRequired
   },
   componentDidMount: function () {
-    var modal = new Foundation.Reveal($('#error-modal'));
-    modal.open();
-  },
-  render: function () {
     var {title, message} = this.props;
-
-    return (
+    var modalMarkup = (
       <div id="error-modal" className="reveal tiny text-center" data-reveal="">
         <h4>{title}</h4>
         <p>{message}</p>
@@ -29,6 +26,20 @@ var ErrorModal = React.createClass({
           </button>
         </p>
       </div>
+    );
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    $(ReactDOM.findDOMNode(this)).html($modal);
+    //react does not work well with 3rd party libraries, so
+    //need to move modal stuff outof the render method.
+    //i.e. Roundation + React = problems.
+    var modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  },
+  render: function () {
+    var {title, message} = this.props;
+    //still need to return divs because it's the minimum requirement.
+    return (
+      <div></div>
     );
   }
 });
